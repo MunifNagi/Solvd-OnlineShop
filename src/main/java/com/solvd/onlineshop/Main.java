b7a3dac6f017ea78ad83232327812ced0fc1dfbf
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-09-12 11:07:06 -0400

    User service

--- src/main/java/com/solvd/onlineshop/Main.java
+++ src/main/java/com/solvd/onlineshop/Main.java
@@ -1,8 +1,16 @@
 package com.solvd.onlineshop;
 
+import com.solvd.onlineshop.entities.User;
+import com.solvd.onlineshop.services.UserService;
+
 public class Main {
     public static void main(String[] args) {
-
+        UserService us = new UserService();
+        us.createUser(new User(1,"Alex","John","A","347-000-0000","Alex@test.com","Alex1234",1));
+        User user = us.getUserByID(1);
+        System.out.println(user);
+        user.setPhone("000-000-0000");
+        us.update(user);
     }
 
 }
---
+++ src/main/java/com/solvd/onlineshop/services/UserService.java
@@ -0,0 +1,45 @@
+package com.solvd.onlineshop.services;
+
+import com.solvd.onlineshop.entities.User;
+import com.solvd.onlineshop.dao.IAddressDAO;
+import com.solvd.onlineshop.dao.IOrderDAO;
+import com.solvd.onlineshop.dao.IUserDAO;
+import com.solvd.onlineshop.dao.mysql.AddressDAO;
+import com.solvd.onlineshop.dao.mysql.OrderDAO;
+import com.solvd.onlineshop.dao.mysql.UserDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+    private IUserDAO userDAO = new UserDAO();
+    private IOrderDAO orderDAO = new OrderDAO();
+    private IAddressDAO addressDAO = new AddressDAO();
+
+    public User getUserByID(long id) {
+        User u = userDAO.getByID(id);
+        if(u==null) {
+            throw new RuntimeException("User with id "+ id + " wasn't found!");
+        }
+        return  u;
+    }
+
+    public void createUser(User user) {
+        this.userDAO.create(user);
+    }
+
+    public List<User> findAllUsers() {
+        List<User> usersList = null;
+        usersList = userDAO.getAllUsers();
+        return usersList;
+    }
+
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    public void remove(long id) {
+        userDAO.remove(id);
+    }
+
+}
