62183960d5176260e4a9880ab774058487fc88ac
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-09-16 00:57:23 -0400

    getting db login from properties file

--- src/main/java/com/solvd/onlineshop/ConnectionPool.java
+++ src/main/java/com/solvd/onlineshop/ConnectionPool.java
@@ -1,31 +1,48 @@
 package com.solvd.onlineshop;
 
+import com.solvd.onlineshop.dao.mysql.AddressDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.imageio.IIOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class ConnectionPool {
-
+    private static final Logger logger = LogManager.getLogger(ConnectionPool.class);
     private static ConnectionPool pool;
-
-
     private static List<Connection> connectionPool = new ArrayList<>(5);
 
     private ConnectionPool(){
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader("src/main/resources/sqlconfig.properties"));
+            String url = properties.getProperty("url");
+            String user = properties.getProperty("username");
+            String password = properties.getProperty("password");
+            for (int i = 0; i < 5; i++) {
+                connectionPool.add(DriverManager.getConnection(url, user, password));
+            }
+        } catch (SQLException e) {
+            logger.error("connection to database failed",e);
+        } catch (FileNotFoundException fnfe) {
+            logger.error("connection to database failed, could not find properties file",fnfe);
+        } catch (IOException ioe) {
+            logger.error("connection to database failed, could not load properties file",ioe);
+        }
     }
+
 
     public static ConnectionPool getInstance(){
         if(pool == null) {
             pool = new ConnectionPool();
-            for (int i = 0; i < 5; i++) {
-                try {
-                    connectionPool.add(createConnection("jdbc:mysql://localhost:3306/OnlineShop", "root", "munif"));
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
         return pool;
     }
@@ -42,7 +59,4 @@ public class ConnectionPool {
         connectionPool.add(connection);
     }
 
-    private static Connection createConnection(String url, String user, String password) throws SQLException {
-        return DriverManager.getConnection(url, user, password);
-    }
 }
